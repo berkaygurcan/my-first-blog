a23599a10a02a0afcfb6c076495ad00d7307d561
berkaygurcan <berkaygurcan141@gmail.com>
2020-02-10 20:35:43 +0300

    Site için CSS ve ayrıntılı blog gönderisi için görünüm ve şablon eklendi.

--- blog/urls.py
+++ blog/urls.py
@@ -2,7 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.post_list, name='post_list'),
-    #Gördüğünüz üzere, ana URL'e post_list adında bir view atıyoruz
+    path('', views.post_list, name='post_list'), #Gördüğünüz üzere, ana URL'e post_list adında  bir view atıyoruz
+    path('post/<int:pk>/',views.post_detail, name = 'post_detail'), #post/<int:pk>/ kısmı bir URL kalıbı belirtir
+    
 ]
-#Son kısım name='post_list', görünümü (view) tanımlamak için kullanılan URL'in adıdır. Bu view'un adı ile aynı olabilir ama tamamen farklı bir şey de olabilir.
+#Son kısım name='post_list', görünümü (view) tanımlamak için kullanılan URL'in adıdır.
+#  Bu view'un adı ile aynı olabilir ama tamamen farklı bir şey de olabilir.
+# <int:pk> - bu bölüm daha zorlayıcı. Django'nun bir tamsayı değeri beklediği ve onu pk adlı bir değişken olarak bir görünüme(view) aktaracağı anlamına gelir.
--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 from django.utils import timezone
 from .models import Post
-
 # Create your views here.
 
 def post_list(request):
@@ -12,4 +11,6 @@ def post_list(request):
 # bir fonksiyon (def) yarattık, 
 # bu fonksiyon girdi olarak request (isteği) alıp, blog/post_list.html template'ini işleyecek olan render fonksiyonunu döndürüyor.
 
-
+def post_detail(request, pk):
+    post = get_object_or_404(Post, pk=pk)
+    return render(request, 'blog/post_detail.html', {'post': post})
