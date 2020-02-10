d71780a48cc95d0e71cc5fbd4ae7681ea8f5ca60
berkaygurcan <berkaygurcan141@gmail.com>
2020-02-10 16:28:31 +0300

    Veritabanından gönderileri göstermek için template'ler değiştirildi.

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,15 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post
 
 # Create your views here.
 
 def post_list(request):
-    return render(request,'blog/post_list.html',{})
+    posts = Post.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+    
 #Burada post_list isimli 
 # bir fonksiyon (def) yarattık, 
 # bu fonksiyon girdi olarak request (isteği) alıp, blog/post_list.html template'ini işleyecek olan render fonksiyonunu döndürüyor.
+
+
