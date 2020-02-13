6eaee4341d6a33112bac3e58b23ff6d47e3c281d
berkaygurcan <berkaygurcan141@gmail.com>
2020-02-13 20:57:57 +0300

    djang ek tutorial bitti

--- blog/admin.py
+++ blog/admin.py
@@ -1,8 +1,8 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post , Comment
 
 # Register your models here.
 
 
 admin.site.register(Post) # Admin sayfasında modelimizi görünür kılabilmek için, modeli admin.site.register(Post) ile kaydetmemiz gerekiyor.
-
+admin.site.register(Comment)
